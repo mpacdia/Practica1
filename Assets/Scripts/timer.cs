f88e609ae7bbc9ed5b89a1fad2269002cd6e6d6e
mpacdia <146739066+mpacdia@users.noreply.github.com>
2023-10-09 10:35:40 +0100

    tiempo
    añadido temporizador

--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int min, seg;
+    public TMP_Text tiempo;
+
+    private float restante;
+    private bool enMarcha;
+
+    private void Awake()
     {
-        
+        restante = (min * 60) + seg;
+        enMarcha = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (enMarcha)
+        {
+            restante -= Time.deltaTime;
+            if(restante < 1)
+            {
+                enMarcha = false;
+            }
+            int tempMin = Mathf.FloorToInt(restante / 60);
+            int tempSeg = Mathf.FloorToInt(restante % 60);
+            tiempo.text = string.Format("{00:00}:{01:00}", tempMin, tempSeg);
+        }
     }
 }
